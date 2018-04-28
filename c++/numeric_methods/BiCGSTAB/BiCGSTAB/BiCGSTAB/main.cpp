c480a4d6c8d5c387b4503ba6b340549fc63c1ae6
Dmitry Gladkov <dmitry.gladkov@intel.com>
2018-04-28 12:08:42 +0300

    c++/numeric methods: Add BiCGSTAB implementation
    Signed-off-by: Dmitry Gladkov <dmitry.gladkov@intel.com>

--- c++/numeric_methods/BiCGSTAB/BiCGSTAB/BiCGSTAB/main.cpp
+++ c++/numeric_methods/BiCGSTAB/BiCGSTAB/BiCGSTAB/main.cpp
@@ -41,6 +41,12 @@ static inline void showCRSMatrix(const CRSMatrix &crs)
 	}
 }
 
+static inline void showVector(double *vector, int size_vector)
+{
+	for (int i = 0; i < size_vector; i++)
+		cout << setw(10) << vector[i] << " ";
+}
+
 class CRS_Matrix {
 	vector<int> non_zero_row_elem_num;
 
@@ -188,6 +194,7 @@ static inline void transposeCRSMatrix(const CRSMatrix &crs_matrix, CRSMatrix &cr
 
 static inline void multiplicationCRSMatrixVector(const CRSMatrix &crs_matrix, const double *vector, double *result_vector)
 {
+#pragma omp parallel for
 	for (int i = 0; i < crs_matrix.n; i++) {
 		result_vector[i] = 0;
 		for (int j = crs_matrix.rowPtr[i]; j < crs_matrix.rowPtr[i + 1]; j++)
@@ -198,27 +205,196 @@ static inline void multiplicationCRSMatrixVector(const CRSMatrix &crs_matrix, co
 static inline double multiplicationVectorVector(double *vector1, double *vector2, int size_vector)
 {
 	double sum = 0.0;
-#pragma omp parallel for simd schedule (static,16) reduction(+:sum) 
+#pragma omp parallel for reduction(+:sum) 
 	for (int i = 0; i < size_vector; i++)
 		sum += vector1[i] * vector2[i];
 	return sum;
 }
 
+static inline void multiplicationVectorScalar(double *vector, int size_vector, double scalar) {
+#pragma omp parallel for
+	for (int i = 0; i < size_vector; i++)
+		vector[i] *= scalar;
+}
+
+static inline bool isEqual(double x, double y, double eps)
+{
+	return (fabs(x - y) < eps) ? true : false;
+}
+
+static inline bool isNotEqual(double x, double y, double eps)
+{
+	return !isEqual(x, y, eps);
+}
+
+static inline void checkResult(const CRSMatrix &crs_matrix, double *b, double *x, double eps) {
+	double *check_vector = new double[crs_matrix.n];
+
+	multiplicationCRSMatrixVector(crs_matrix, x, check_vector);
+
+	bool fcheck = true;
+	for (int i = 0; i < crs_matrix.n; i++) {
+		if (isNotEqual(check_vector[i], b[i], eps)) {
+			fcheck = false;
+			break;
+		}
+	}
+
+	if (crs_matrix.n <= 10) {
+		cout << "CRS Matrix A:" << endl;
+		showCRSMatrix(crs_matrix);
+		cout << endl;
+
+		cout << "Vector b:" << endl;
+		showVector(b, crs_matrix.n);
+		cout << endl;
+
+		cout << "Vector x:" << endl;
+		showVector(x, crs_matrix.n);
+		cout << endl;
+
+		if (!fcheck) {
+			cout << "Check vector (A*x):" << endl;
+			showVector(check_vector, crs_matrix.n);
+			cout << endl;
+		}
+	}
+
+	cout << "Check of algorithm: ";
+	if (fcheck)
+		cout << "Successful" << endl;
+	else
+		cout << "Failed" << endl;
+
+	delete[] check_vector;
+}
+
+void SLE_Solver_CRS_BICG(CRSMatrix &A, double *b, double eps, int max_iter, double *x, int &count) {
+
+	CRSMatrix At;
+	// Arrays to store discrepancies for current and next approximations 
+	double *R;
+	double *biR;
+	double *nR;
+	double *nbiR;
+
+	// Arrays to store the current and next vector method step directions
+	double *P;
+	double *biP;
+	double *nP;
+	double *nbiP;
+
+	// Arryas to store the product of multiplication matrix and direction vector and bi-conjugate to it
+	double *multAP;
+	double *multAtbiP;
+
+	// Coefficients of computational formulas
+	double alfa, beta;
+
+	// Numerator and denominator of beta and alfa coefficients
+	double numerator, denominator;
+	// Variables to calculate the accuracy of the current approximation
+	double check, norm;
+
+#pragma omp parallel sections
+	{
+#pragma omp section
+		{
+			initCRSMatrix(At, A.n, A.nz);
+			transposeCRSMatrix(A, At);
+		}
+#pragma omp section
+		{
+			R = new double[A.n];
+			biR = new double[A.n];
+			nR = new double[A.n];
+			nbiR = new double[A.n];
+
+			P = new double[A.n];
+			biP = new double[A.n];
+			nP = new double[A.n];
+			nbiP = new double[A.n];
+
+			multAP = new double[A.n];
+			multAtbiP = new double[A.n];
+		}
+#pragma omp section
+		{
+			norm = sqrt(multiplicationVectorVector(b, b, A.n));
+		}
+	}
+
+#pragma omp parallel for
+	for (int i = 0; i < A.n; i++)
+		x[i] = 1.0;
+	multiplicationCRSMatrixVector(A, x, multAP);
+
+#pragma omp parallel for
+	for (int i = 0; i < A.n; i++)
+		R[i] = biR[i] = P[i] = biP[i] = b[i] - multAP[i];
+
+	for (count = 0; count < max_iter; count++) {
+		multiplicationCRSMatrixVector(A, P, multAP);
+		multiplicationCRSMatrixVector(At, biP, multAtbiP);
+		numerator = multiplicationVectorVector(biR, R, A.n);
+		denominator = multiplicationVectorVector(biP, multAP, A.n);
+		alfa = numerator / denominator;
+
+#pragma omp parallel for
+		for (int i = 0; i < A.n; i++) {
+			nR[i] = R[i] - alfa * multAP[i];
+			nbiR[i] = biR[i] - alfa * multAtbiP[i];
+		}
+
+		denominator = numerator;
+		numerator = multiplicationVectorVector(nbiR, nR, A.n);
+		beta = numerator / denominator;
+
+#pragma omp parallel for
+		for (int i = 0; i < A.n; i++) {
+			nP[i] = nR[i] + beta * P[i];
+			nbiP[i] = nbiR[i] + beta * biP[i];
+		}
+
+		check = sqrt(multiplicationVectorVector(R, R, A.n)) / norm;
+		if (check < eps)
+			break;
+
+#pragma omp parallel for
+		for (int i = 0; i < A.n; i++)
+			x[i] += alfa * P[i];
+
+		std::swap(R, nR);
+		std::swap(P, nP);
+		std::swap(biR, nbiR);
+		std::swap(biP, nbiP);
+	}
+
+	delete[] R, biR, nR, nbiR;
+	delete[] P, biP, nP, nbiP;
+	delete[] multAP;
+	delete[] multAtbiP;
+}
+
 int main(int argc, char **argv)
 {
+	int max_iter = 10, count = 10;
+	double eps = 0.001;
 	CRS_Matrix crs_matrix;
-	CRSMatrix crs_matrix_t;
 	double *b = new double[crs_matrix.getDim()];
 	double *x = new double[crs_matrix.getDim()];
 
+	memset(x, 0, sizeof(double) * crs_matrix.getDim());
 	initVector(b, crs_matrix.getDim(), UCHAR_MAX);
 
-	initCRSMatrix(crs_matrix_t, crs_matrix.getDim(), crs_matrix.getNonZeroElemNumber());
-	transposeCRSMatrix(crs_matrix.crs, crs_matrix_t);
+	SLE_Solver_CRS_BICG(crs_matrix.crs, b, eps, max_iter, x, count);
 
 	cout << crs_matrix;
 	cout << endl;
-	showCRSMatrix(crs_matrix_t);
+
+	checkResult(crs_matrix.crs, b, x, eps);
 
 	getchar();
+
+	delete[] b, x;
 }
